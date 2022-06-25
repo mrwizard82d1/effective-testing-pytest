08c2c7fda3a554af8c428ee799956b449710b547
laj <larry.jones@reveal-energy.com>
2022-06-25 17:00:30 -0500

    A simple test using unittest

---
+++ test_with_unittest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from unittest import TestCase
+
+
+class TryTesting(TestCase):
+    def test_always_passes(self):
+        self.assertTrue(True)
+
+    def test_always_fails(self):
+        self.assertTrue(False)
